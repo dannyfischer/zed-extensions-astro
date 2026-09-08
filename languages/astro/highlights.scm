a60a8346ce602a6383998eff022f08d572ee3455
Danny Fischer <3680694+dannyfischer@users.noreply.github.com>
2026-09-09 00:55:44 +0200

    chore(highlighting): apply local Astro syntax preferences

--- languages/astro/highlights.scm
+++ languages/astro/highlights.scm
@@ -1,21 +1,25 @@
-(tag_name) @tag
+; Components: <MyComponent>
+((tag_name) @tag.component
+  (#match? @tag.component "^[A-Z]"))
+
+; Native HTML: <div>, <span>, ...
+((tag_name) @tag
+  (#match? @tag "^[a-z]"))
 
 (erroneous_end_tag_name) @keyword
 
 (doctype) @tag.doctype
 
-(attribute_name) @property
-
-(attribute_value) @string
-
-(comment) @comment
+(attribute_name) @attribute.jsx
 
 [
   (attribute_value)
   (quoted_attribute_value)
 ] @string
 
-"=" @operator
+(comment) @comment
+
+"=" @punctuation.delimiter.jsx
 
 [
   "{"
@@ -27,4 +31,4 @@
   ">"
   "</"
   "/>"
-] @tag.delimiter
+] @punctuation.bracket.jsx
